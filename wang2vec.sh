f5edbf9aeace10a8ac94ba677e4088c70459fcc0
ju-resplande <julianarsg13@gmail.com>
2022-12-14 17:20:39 -0300

    no binary

--- wang2vec.sh
+++ wang2vec.sh
@@ -2,5 +2,5 @@ DATA_DIR='../../data/wikipedia.pt.nilc'
 
 cd src/wang2vec
 make
-./word2vec -train ${DATA_DIR} -output ../../models/wang2vec.model \
- -type 0 -size 50 -window 5 -negative 10 -nce 0 -hs 0 -sample 1e-4 -threads 1 -binary 1 -iter 5 -cap 0
+./word2vec -train ${DATA_DIR} -output ../../models/wang2vec.kv \
+ -type 0 -size 50 -window 5 -negative 10 -nce 0 -hs 0 -sample 1e-4 -threads 1 -binary 0 -iter 5 -cap 0
